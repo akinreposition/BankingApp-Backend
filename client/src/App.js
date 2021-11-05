ddda6f11454d7d6a1ca778b0756adaec2b264ce2
akinrepostion <odekuakinlolu@gmail.com>
2021-11-05 14:38:06 +0100

    error fix: dependency now installed in client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, {Fragment}  from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layouts/navbar/Navbar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
 import './App.css';
 const App = () => {
   return (
-    <div className="App">
-      <Navbar/>
-      Account Keeper
-    </div>
+    <Router>
+      <Fragment>
+        <Navbar/>
+        <div className="container">
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/about' component={About} />
+          </Switch>
+        </div>
+      </Fragment>
+    </Router>
   );
 }
 
---
+++ client/src/components/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            <h1>About This App</h1>
+            <p className="my-1">
+                  This is a Account Keeping Application.
+                  Used to keep account details of you or your clients and customer
+            </p>
+            <p className="bg-dark p">
+                <strong>Version:</strong> 1.0.0
+            </p>
+        </div>
+    )
+}
